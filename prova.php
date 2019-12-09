b84ac0ddb8e6cd863611d5487ef9770d324e2b9f
Andrea Coradi <andreacoradi30@gmail.com>
2019-12-09 12:51:54 +0100

    d

--- prova.php
+++ prova.php
@@ -16,13 +16,36 @@
 	// var_dump($mezzi);
 	//var_dump($routes);
 	echo "<p>Da " . $start . " a " . $end . " dista " . $routes[$start][$end][$mezzi[0]]["distanza"];
+
+	$a = $routes[$start][$end];
+	$t = 0;
+	$key = "distanza";
+	if($tipo == "shortest") {
+		$t = $a[$mezzi[0]];
+	} elseif ($tipo == "fastest") {
+		$key = "tempo";
+	} elseif ($tipo == "cheapest") {
+		$key = "costo";
+	}
+
+	$best = $routes[$start][$end][$mezzi[0]];
+	$m = $mezzi[0];
+
 	foreach($mezzi as $mezzo) {
 		$linea = $routes[$start][$end][$mezzo];
+
 		if($linea != NULL) {
-			echo "<p>Andando in " . $mezzo . " costa " . $linea["costo"] . " e ci metti " . $linea["tempo"] ."</p>";
+			echo $linea[$key] . " " . $t;
+			if($linea[$key] < $t) {
+				$best = $linea;
+				$m = $mezzo;
+			}
+			//echo "<p>Andando in " . $mezzo . " costa " . $linea["costo"] . " e ci metti " . $linea["tempo"] ."</p>";
 			//echo $routes[$start][$end][$mezzo];
 		}
 	}
+
+	echo "<p>Andando in " . $m . " costa " . $best["costo"] . " e ci metti " . $best["tempo"] . " in distanza ". $best["distanza"] . "</p>";
 
 ?>
 </body>
