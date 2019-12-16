c2bf403047f7447f88ded38dd215e664e1901f3d
Lorenco Malaj <lorenhxz@MacBook-Air-di-Lorenco-3.local>
2019-12-16 12:05:54 +0100

    c

--- main.php
+++ main.php
@@ -161,13 +161,9 @@
 			/*$durata = strval(number_format((float)$value["tempo"] / 60, 2, '.', ''));
 			$durata = str_replace(".", ":", $durata);*/
 			$durata = gmdate("i:s", $value["tempo"]);
-<<<<<<< HEAD
-			
-=======
 
 			$icona = "<img src='./icons/$key.svg' width='20' height='20'>";
 			echo "<p>$icona $c1 to $c2 by $key: $" . $value["costo"] . ", duration " . $durata . " hours</p>";
->>>>>>> 45ed962e9c2927ac32d4566a344c92b23b14f464
 			$costo += $value["costo"];
 		}
 		//var_dump($routes_temp[$c1][$c2][array_keys($routes_temp[$c1][$c2])]);
